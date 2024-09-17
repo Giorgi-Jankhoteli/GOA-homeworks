44cccd4794aeab6ce93e58edd5d0dd987356c4c3
Giorgi-Jankhoteli <giorgijankhoteli2@gmail.com>
2024-09-17 18:36:51 +0400

    commit

---
+++ Day 04/classwork/classwork 1.py
@@ -0,0 +1,50 @@
+price = 150 #integer
+print(price)
+
+price = 5
+amount = 3
+print(price * amount)
+
+price = 5
+amount = 6
+total = price * amount
+print(total)
+
+price = 99
+price = 100
+print(price)
+
+price = 99
+
+print(price) # 99
+
+price = 100
+
+print(price) # 100
+
+name = "Tom" # string
+level = 14 # integer
+
+print(name)
+
+level = level + 1
+         #14
+
+print(level)
+
+message = "Debugging"
+print(message)
+
+# parentheses - ფრჩხილები
+
+salary = 70000
+
+print(salary)
+
+age = 32
+
+role = "product manager"
+
+print(role)
+
+# Declare - შექმნა
---
+++ Day 04/classwork/classwork 2.py
@@ -0,0 +1,44 @@
+num1 = 15
+num2 = 25
+sum = num1 + num2
+print(sum)
+
+price = 50
+price = 60
+print(price)
+
+# debugging-არის კოდში error-ის პოვნა და მისი გასწორება
+
+
+# შეასწორეთ შეცდომები
+
+# Excercise 1:
+name = "Hello, World!"
+print("Hello, World!")
+
+# Excercise 2:
+greeting = "Good Morning"
+print(greeting)
+
+# Excercise 3:
+first_name = "Alice"
+last_name = "Smith"
+print("Name:" + " " + first_name + " " + last_name)
+
+# Excercise 4:
+number = 50
+print("The number is:" + " " + str(number))
+
+# Excercise 5:
+name = "George"
+print("Hello" + " " + name)
+
+# Excercise 6:
+age = 25
+print("I am" + " " + str(age) + " " + "years old.")
+
+# Excercise 7:
+num1 = 10
+num2 = 20
+result = num1 + num2
+print(result)
