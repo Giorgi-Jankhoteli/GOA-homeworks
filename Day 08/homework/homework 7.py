74498c852906f368de7881ded409fe15bfb3a5b0
Giorgi-Jankhoteli <giorgijankhoteli2@gmail.com>
2024-10-02 13:36:45 +0400

    commit

--- Day 08/homework/homework 7.py
+++ Day 08/homework/homework 7.py
@@ -5,6 +5,6 @@ P_and_not_Q = P and not Q
 not_P_and_Q = P or not Q
 P_xor_Q = (P and not Q) or (not P and Q)
 
-print("P_and_not_Q:", P_and_not_Q)
-print("not_P_and_Q", not_P_and_Q)
-print("P_xor_Q:", P_xor_Q)
+print(P_and_not_Q)
+print(not_P_and_Q)
+print(P_xor_Q)
