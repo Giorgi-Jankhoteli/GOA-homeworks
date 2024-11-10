6a30e397b30c37212032d2d131f566cb4fc77851
Giorgi-Jankhoteli <giorgijankhoteli2@gmail.com>
2024-11-10 13:04:12 +0400

    commit

---
+++ Level 24/classwork/classwork 1.py
@@ -0,0 +1,13 @@
+# append
+# pop
+# slice
+
+
+my_list = ["giorgi", "gabrieli", "giorgi", "luka", 'daviti']
+
+my_list.append(8)
+my_list.remove("giorgi")
+my_list.remove("giorgi")
+my_list.pop(0)
+
+print(my_list)
